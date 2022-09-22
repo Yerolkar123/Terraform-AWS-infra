faf82ce25c358fafbe59c4f72f0293b3adaec3b2
Yerolkar123 <93875322+Yerolkar123@users.noreply.github.com>
2022-09-22 13:13:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,15 @@ terraform {
   }
 }
 
-resource "aws_vpc" "my-vpc"{
-  aws_region         = "us-east-1"
-  aws_vpc_cidr_block = "10.0.0.0/16"
-  aws_vpc_name       = "my-vpc"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }

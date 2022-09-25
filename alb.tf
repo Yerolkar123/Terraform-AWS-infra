ede4721e1313b93b9001301ab2a611789ef6dd51
Yerolkar123 <93875322+Yerolkar123@users.noreply.github.com>
2022-09-26 01:15:05 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -41,7 +41,7 @@ resource "aws_lb_listener" "front_end" {
 
 resource "aws_lb_target_group_attachment" "ec2_attach" {
   target_group_arn = aws_lb_target_group.target_group.arn
-  target_id        = aws_instance.web-server.id
+  target_id        =  aws_instance.web-server[count.index].id
   count            = length(aws_instance.web-server)
 }
 

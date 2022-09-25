2287956900731781fa06b0dc8885c42bb5ad7a7b
Yerolkar123 <93875322+Yerolkar123@users.noreply.github.com>
2022-09-25 23:30:38 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "elb-dns-name" {
     value = "${aws_lb.application_lb.dns_name}" 
 }
-output igw_id {
-  value = aws_internet_gateway.igw.id
+output "igw_id" {
+  value = aws_internet_gateway.demo_igw.id
 }
 output "vpc_id" {
     value = aws_vpc.demo_vpc.id

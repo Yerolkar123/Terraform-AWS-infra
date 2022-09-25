d3755ccb9e7f6a52f036338637e5d66ae364c097
Yerolkar123 <93875322+Yerolkar123@users.noreply.github.com>
2022-09-25 22:59:51 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,28 +3,17 @@ variable "region" {
     default = "us-east-1"
 }
 variable "image_id" {
-    default = ""
+    default = "0b3e423be2b1ed909"
 }
-variable "key_name" {
-   default = "demo-ec2"
-}
-variable "var.machine_type" {
+variable "instance_type" {
     default = "t2.micro"
 }
 variable "vpc_cidr" {
     default = "10.0.0.0/30"
 }
-variable "Public_cidr" {
+variable "Public_cidr_block" {
     default = "10.0.0.14/30" 
 }
-variable "Private_cidr" {
+variable "Private_cidr_block" {
     default = 	"10.0.0.13/30"
 }
-# Bucket  name 
-/*
-variable "s3_bucket_name" {
-    description = "s3_bucket_name"
-    type = string
-    default = "202i0ike"
-}
-*/

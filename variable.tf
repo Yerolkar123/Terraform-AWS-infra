5316151f0df4e647690921853f917a6093b40286
Yerolkar123 <93875322+Yerolkar123@users.noreply.github.com>
2022-09-26 01:27:25 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "region"  {
     default = "us-east-1"
 }
 variable "image_id" {
-    default = "0b3e423be2b1ed909"
+    default = "ami-0b3e423be2b1ed909"
 }
 variable "instance_type" {
     default = "t2.micro"
@@ -12,7 +12,7 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 variable "Public_cidr_block" {
-    default = "10.0.0.0/16" 
+    default = "10.0.0.0/24" 
 }
 variable "Private_cidr_block" {
     default = 	"10.0.1.0/24"

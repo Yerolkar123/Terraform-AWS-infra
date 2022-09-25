b3707b612315eada5afa03157fd0d800caa59116
Yerolkar123 <93875322+Yerolkar123@users.noreply.github.com>
2022-09-26 00:48:12 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -9,11 +9,11 @@ variable "instance_type" {
     default = "t2.micro"
 }
 variable "vpc_cidr" {
-    default = "10.0.0.0/30"
+    default = "10.0.0.0/16"
 }
 variable "Public_cidr_block" {
-    default = "10.0.0.14/30" 
+    default = "10.0.0.14/24" 
 }
 variable "Private_cidr_block" {
-    default = 	"10.0.0.13/30"
+    default = 	"10.0.1.0/24"
 }

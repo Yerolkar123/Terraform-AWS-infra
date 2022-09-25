9f86afa2388044e469ddfb0af3cd9fc1d5c88497
Yerolkar123 <93875322+Yerolkar123@users.noreply.github.com>
2022-09-26 00:51:44 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -12,7 +12,7 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 variable "Public_cidr_block" {
-    default = "10.0.0.14/24" 
+    default = "10.0.0.0/16" 
 }
 variable "Private_cidr_block" {
     default = 	"10.0.1.0/24"

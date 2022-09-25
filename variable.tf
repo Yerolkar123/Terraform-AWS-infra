5330450c01e9ba195ce533287175f4bd31b975ff
Yerolkar123 <93875322+Yerolkar123@users.noreply.github.com>
2022-09-26 04:09:02 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -12,11 +12,11 @@ variable "vpc_cidr" {
     default = "172.31.0.0/20"
 }
 variable "Public_subA_cidr_block" {
-    default = "172.31.32.0/20" 
+    default = "172.31.32.0/32" 
 }
 variable "Public_subB_cidr_block" {
-    default = "172.31.16.0/20" 
+    default = "172.31.16.0/32" 
 }
 variable "Private_cidr_block" {
-    default = 	"172.31.4.0/20"
+    default = 	"172.31.32.5/32"
 }
